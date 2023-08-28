9e8ab049593abf1bc4e5f33e8b0ea6bd8003e67f
Deep Patel <marsonia.deep@gmail.com>
2023-08-28 13:59:20 +0530

    locations db creation ok init

---
+++ database/migrations/2023_08_28_082330_create_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id("location_id");
+            $table->string("name" ,255);
+
+            $table->string("latitude" , 20);
+            $table->string("longitude" , 20);
+
+            $table->string("nav_url");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
