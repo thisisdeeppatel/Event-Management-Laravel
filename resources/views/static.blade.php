85074b100c9a37a85884a37a82486d45c25798bc
Deep Patel <marsonia.deep@gmail.com>
2023-08-28 13:21:41 +0530

    Static Files OK

---
+++ resources/views/static.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="{{url('/')}}/static/css/bootstrap.min.css" />
+<link rel="stylesheet" href="{{url('/')}}/static/css/jquery.dataTables.css" />
+<script src="{{url('/')}}/static/js/bootstrap.bundle.min.js"></script>
+<script src="{{url('/')}}/static/js/jquery-3.6.4.min.js"></script>
+<script src="{{url('/')}}/static/js/jquery.dataTables.js"></script>
